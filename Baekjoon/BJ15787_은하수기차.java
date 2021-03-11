269b159adb3923a309569771d69d2503792f8155
“wonjwi” <wonji616@gmail.com>
2021-03-12 02:20:02 +0900

    fix BJ15787

--- Baekjoon/BJ15787_은하수기차.java
+++ Baekjoon/BJ15787_은하수기차.java
@@ -1,11 +1,10 @@
 package week06;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BJ15787_은하수기차 {
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine());
@@ -18,28 +17,25 @@ public class BJ15787_은하수기차 {
 		int M = Integer.parseInt(st.nextToken());
 		for (int t = 0; t < M; t++) {
 			st = new StringTokenizer(in.readLine());
-			int i = 0, x = 0;
+			int i, x;
 			char order = st.nextToken().charAt(0);
+			i = Integer.parseInt(st.nextToken());
 			switch (order) {
 			case '1':
-				i = Integer.parseInt(st.nextToken());
 				x = Integer.parseInt(st.nextToken());
 				train[i][x] = true;
 				break;
 			case '2':
-				i = Integer.parseInt(st.nextToken());
 				x = Integer.parseInt(st.nextToken());
 				train[i][x] = false;
 				break;
 			case '3':
-				i = Integer.parseInt(st.nextToken());
 				for (int j = 20; j > 1; j--) {
 					train[i][j] = train[i][j-1];
 				}
 				train[i][1] = false;
 				break;
 			case '4':
-				i = Integer.parseInt(st.nextToken());
 				for (int j = 1; j < 20; j++) {
 					train[i][j] = train[i][j+1];
 				}
@@ -48,22 +44,16 @@ public class BJ15787_은하수기차 {
 			}
 		}
 		// 은하수를 건널 수 있는 기차의 수 구하기
-		int cnt = 1;
-		for (int i = 2; i <= N; i++) {
-			boolean isVaild = false;
-			for (int j = 1; j < i; j++) {
-				isVaild = false;
-				for (int k = 1; k <= 20; k++) {
-					// 승객 상태가 하나라도 다르면 건널 수 있음
-					if (train[i][k] != train[j][k]) {
-						isVaild = true;
-					}
-				}
-				if (!isVaild) break;
+		Set<String> set = new HashSet<String>();
+		StringBuilder sb;
+		for (int i = 1; i <= N; i++) {
+			sb = new StringBuilder();
+			for (int x = 1; x <= 20; x++) {
+				sb.append(train[i][x] ? '1' : '0');
 			}
-			if (isVaild) cnt++;
+			set.add(sb.toString());
 		}
-		System.out.println(cnt);
+		System.out.println(set.size());
 	}
-
+	
 }

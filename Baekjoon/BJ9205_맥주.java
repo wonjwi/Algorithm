aa6500d7d1b685d37493df2c1252b4cc16395b7c
“wonjwi” <wonji616@gmail.com>
2021-03-26 02:59:09 +0900

    fix BJ9205

--- Baekjoon/BJ9205_맥주.java
+++ Baekjoon/BJ9205_맥주.java
@@ -54,3 +54,54 @@ public class BJ9205_맥주 {
 	}
 	
 }
+
+//import java.io.*;
+//import java.util.StringTokenizer;
+//
+//public class BJ9205_맥주 {
+//
+//	public static void main(String[] args) throws NumberFormatException, IOException {
+//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = null;
+//		StringBuilder sb = new StringBuilder();
+//		
+//		int T = Integer.parseInt(in.readLine());
+//		for (int tc = 0; tc < T; tc++) {
+//			// 상근이네, 편의점, 락페스티벌 좌표 입력
+//			int N = Integer.parseInt(in.readLine());
+//			int[][] coords = new int[N+2][2];
+//			for (int i = 0; i < N+2; i++) {
+//				st = new StringTokenizer(in.readLine());
+//				coords[i][0] = Integer.parseInt(st.nextToken());
+//				coords[i][1] = Integer.parseInt(st.nextToken());
+//			}
+//			
+//			// 맥주 한 박스(20개) * 50미터에 한 병 => 1000미터까지
+//			int INF = 10000;
+//			int[][] dist = new int[N+2][N+2];
+//			for (int i = 0; i < N+2; i++) {
+//				for (int j = 0; j < N+2; j++) {
+//					// 자기 위치로 돌아올 필요 X
+//					if (i == j || dist[i][j] != 0) continue;
+//					int tmp = Math.abs(coords[i][0]-coords[j][0]) + Math.abs(coords[i][1]-coords[j][1]);
+//					if (tmp <= 1000) dist[i][j] = dist[j][i] = 1;
+//					else dist[i][j] = dist[j][i] = INF;
+//				}
+//			}
+//			
+//			// 경유지 -> 출발지 -> 도착지
+//			for (int k = 0; k < N+2; k++) {
+//				for (int i = 0; i < N+2; i++) {
+//					for (int j = 0; j < N+2; j++) {
+//						if (dist[i][j] > dist[i][k]+dist[k][j]) {
+//							dist[i][j] = dist[i][k]+dist[k][j];
+//						}
+//					}
+//				}
+//			}
+//			sb.append(dist[0][N+1] == INF ? "sad" : "happy").append("\n");
+//		}
+//		System.out.println(sb.toString());
+//	}
+//	
+//}

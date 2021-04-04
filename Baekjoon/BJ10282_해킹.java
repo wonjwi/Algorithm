ee31e288646d90d4f236015c1647e2e4999d9c0d
“wonjwi” <wonji616@gmail.com>
2021-04-04 12:12:21 +0900

    fix BJ10282

--- Baekjoon/BJ10282_해킹.java
+++ Baekjoon/BJ10282_해킹.java
@@ -5,6 +5,15 @@ import java.util.*;
 
 public class BJ10282_해킹 {
 	
+	static class Computer {
+		int node, time;
+		public Computer(int node, int time) {
+			this.node = node;
+			this.time = time;
+		}
+	}
+	static int cnt, result;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -16,9 +25,9 @@ public class BJ10282_해킹 {
 			int c = Integer.parseInt(st.nextToken()); // 해킹당한 컴퓨터 번호
 			
 			// 의존성 정보 입력
-			Map<Integer, Integer>[] adjList = new HashMap[n+1];
+			List<Computer>[] adjList = new ArrayList[n+1];
 			for (int i=1; i<=n; i++) {
-				adjList[i] = new HashMap<Integer, Integer>();
+				adjList[i] = new ArrayList<Computer>();
 			}
 			for (int i = 0; i < d; i++) {
 				// a가 b를 의존, b 감염 s초 후 a도 감염됨
@@ -26,40 +35,46 @@ public class BJ10282_해킹 {
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int s = Integer.parseInt(st.nextToken());
-				adjList[b].put(a, s);
+				adjList[b].add(new Computer(a, s));
 			}
-			// 입력된 정보로 컴퓨터 감염 시작
-			int[] distance = new int[n+1];
-			boolean[] visited = new boolean[n+1];
-			Arrays.fill(distance, Integer.MAX_VALUE);
-			distance[c] = 0; // c부터 감염 시작
-			
-			int cnt = 0, idx = 0;
-			for (int i = 1; i <= n; i++) {
-				int min = Integer.MAX_VALUE;
-				int curr = 0;
-				// 감염되지 않은 컴퓨터 중 감염에 걸리는 시간이 짧은 곳 선택
-				for (int j = 1; j <= n; j++) {
-					if (!visited[j] && min > distance[j]) {
-						min = distance[j];
-						curr = j;
-					}
-				}
-				// 감염이 끝나면 종료
-				if (curr == 0) break;
-				// 감염될 컴퓨터가 있으면 계속 진행
-				visited[curr] = true;
-				cnt++; idx = curr;
-				// curr을 경유지로 하는 감염되지 않은 다른 컴퓨터로의 감염 시간 따져보기
-				for (int j = 1; j <= n; j++) {
-					if (visited[j] || !adjList[curr].containsKey(j)) continue;
-					distance[j] = Math.min(distance[j], min+adjList[curr].get(j));
-				}
-			}
+			// 컴퓨터 c부터 컴퓨터 감염 시작
+			cnt = 1; result = 0;
+			dijkstra(adjList, n, c);
 			// 감염되는 컴퓨터 수와 마지막 감염까지 걸리는 시간
-			sb.append(cnt).append(" ").append(distance[idx]).append("\n");
+			sb.append(cnt).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb.toString());
+	}
+
+	private static void dijkstra(List<Computer>[] adjList, int n, int start) {
+		// 컴퓨터 start부터 감염 시작
+		int[] distance = new int[n+1];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		distance[start] = 0; 
+		
+		// 감염에 걸리는 시간이 짧은 곳부터 삽입
+		PriorityQueue<Computer> pq = new PriorityQueue<Computer>((c1, c2) -> c1.time-c2.time);
+		pq.add(new Computer(start, 0));
+		
+		// 감염이 끝날 때까지 반복
+		while (!pq.isEmpty()) {
+			Computer tmp = pq.poll();
+			int curr = tmp.node;
+			// 마지막 감염 시간 갱신
+			result = Math.max(result, distance[curr]);
+			
+			// curr을 경유지로 하는 다른 컴퓨터로의 감염 시간 따져보기
+			for (Computer com : adjList[curr]) {
+				int next = com.node, time = com.time;
+				if (distance[next] > distance[curr]+time) {
+					// 첫 방문이면 감염 컴퓨터 수 카운트
+					if (distance[next] == Integer.MAX_VALUE) cnt++;
+					
+					distance[next] = distance[curr]+time;
+					pq.add(new Computer(next, distance[next]));
+				}
+			}
+		}
 	}
 	
 }

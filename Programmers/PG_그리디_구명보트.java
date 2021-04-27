0bf1ba045a496abaaa70b4f5bdf845aac8c5f558
“wonjwi” <wonji616@gmail.com>
2021-04-27 23:48:02 +0900

    fix Programmers 구명보트

--- Programmers/PG_그리디_구명보트.java
+++ Programmers/PG_그리디_구명보트.java
@@ -32,53 +32,54 @@ public class PG_그리디_구명보트 {
         }
         return answer;
     }
-	
-//	static public int solution(int[] people, int limit) {
-//        int N = people.length;
-//        // 몸무게별 사람수 카운팅
-//        int min = 242; int max = 39;
-//        int[] cnt = new int[limit+1];
-//        for (int i=0; i<N; i++) {
-//            min = Math.min(min, people[i]);
-//            max = Math.max(max, people[i]);
-//            cnt[people[i]]++;
-//        }
-//        // 여러 명을 태울 수 있는지 확인
-//        int k = limit/min;
-//        while (true) {
-//            if (k == 1) break;
-//            if (min*k > limit) {
-//                k--; continue;
-//            }
-//            while (min+max > limit) {
-//                for (int i=max-1; i>=min; i--) {
-//                    max = i;
-//                    if (cnt[i] != 0) break;
-//                }
-//            }
-//            if (min == max) {
-//                N -= (cnt[min]/2);
-//                break;
-//            }
-//            if (cnt[min] < cnt[max]) {
-//                N -= cnt[min];
-//                cnt[max] -= cnt[min];
-//                cnt[min] = 0;
-//                for (int i=min+1; i<=max; i++) {
-//                    min = i;
-//                    if (cnt[i] != 0) break;
-//                }
-//            } else {
-//                N -= cnt[max];
-//                cnt[min] -= cnt[max];
-//                cnt[max] = 0;
-//                for (int i=max-1; i>=min; i--) {
-//                    max = i;
-//                    if (cnt[i] != 0) break;
-//                }
-//            }
-//        }
-//        return N;
-//    }
-	
+	/*
+	static public int solution(int[] people, int limit) {
+        int N = people.length;
+        // 몸무게별 사람수 카운팅
+        int min = 242; int max = 39;
+        int[] cnt = new int[limit+1];
+        for (int i=0; i<N; i++) {
+            min = Math.min(min, people[i]);
+            max = Math.max(max, people[i]);
+            cnt[people[i]]++;
+        }
+        // 두 사람을 태울 수 있는지 확인
+        while (true) {
+        	// 가장 가벼운 두 명도 태울 수 없다면
+            if (min*2 > limit) break;
+            // 두 사람을 태울 수 있을 때까지 max 값 갱신
+            while (min+max > limit) {
+                for (int i=max-1; i>=min; i--) {
+                    max = i;
+                    if (cnt[i] != 0) break;
+                }
+            }
+            // 두 사람씩 태울 수 있는 만큼 빼기
+            if (min == max) {
+                N -= (cnt[min]/2);
+                break;
+            }
+            if (cnt[min] < cnt[max]) {
+                N -= cnt[min];
+                cnt[max] -= cnt[min];
+                cnt[min] = 0;
+                // 새로운 min 값 찾기
+                for (int i=min+1; i<=max; i++) {
+                    min = i;
+                    if (cnt[i] != 0) break;
+                }
+            } else {
+                N -= cnt[max];
+                cnt[min] -= cnt[max];
+                cnt[max] = 0;
+                // 새로운 max 값 찾기
+                for (int i=max-1; i>=min; i--) {
+                    max = i;
+                    if (cnt[i] != 0) break;
+                }
+            }
+        }
+        return N;
+    }
+    */
 }

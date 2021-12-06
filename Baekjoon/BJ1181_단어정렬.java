83b4dedfca5cf2a9459684c92d0ee2265f4ff2c8
wonjwi <wonji616@gmail.com>
2021-12-07 00:04:51 +0900

    Update BOJ 1181

--- Baekjoon/BJ1181_단어정렬.java
+++ Baekjoon/BJ1181_단어정렬.java
@@ -22,17 +22,14 @@ public class BJ1181_단어정렬 {
 		ArrayList<String> list = new ArrayList<String>(words);
 
 		Collections.sort(list, new Comparator<String>() {
-
 			@Override
 			public int compare(String o1, String o2) {
-				if (o1.length() > o2.length())
-					return 1;
-				else if (o1.length() < o2.length())
-					return -1;
-				else
+				if (o1.length() == o2.length()) {
 					return o1.compareTo(o2);
+				} else {
+					return o1.length() - o2.length();
+				}
 			}
-
 		});
 
 		// 조건에 맞게 출력

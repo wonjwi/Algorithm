91da74de7a4ebd86189df8d9c09b826a22a32096
wonjwi <wonji616@gmail.com>
2022-02-18 00:28:12 +0900

    Update BOJ 11723

--- Baekjoon/BJ11723_집합.java
+++ Baekjoon/BJ11723_집합.java
@@ -15,31 +15,33 @@ public class Main {
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(in.readLine());
+			char oper = st.nextToken().charAt(1);
 
-			switch (st.nextToken()) {
-			case "add":
-				num[Integer.parseInt(st.nextToken())] = true;
-				break;
-			case "remove":
-				num[Integer.parseInt(st.nextToken())] = false;
-				break;
-			case "check":
-				sb.append(num[Integer.parseInt(st.nextToken())] ? 1 : 0).append("\n");
-				break;
-			case "toggle":
-				int x = Integer.parseInt(st.nextToken());
-				num[x] = !num[x];
-				break;
-			case "all":
+			if (oper == 'l') {
 				for (int j = 1; j <= 20; j++) {
 					num[j] = true;
 				}
-				break;
-			case "empty":
+			} else if (oper == 'm') {
 				for (int j = 1; j <= 20; j++) {
 					num[j] = false;
 				}
-				break;
+			} else {
+				int x = Integer.parseInt(st.nextToken());
+
+				switch (oper) {
+				case 'd':
+					num[x] = true;
+					break;
+				case 'e':
+					num[x] = false;
+					break;
+				case 'h':
+					sb.append(num[x] ? 1 : 0).append("\n");
+					break;
+				case 'o':
+					num[x] = !num[x];
+					break;
+				}
 			}
 		}
 
@@ -47,3 +49,47 @@ public class Main {
 	}
 
 }
+
+// 비트마스킹
+//public class Main {
+//
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = null;
+//		StringBuilder sb = new StringBuilder();
+//
+//		int M = Integer.parseInt(in.readLine());
+//		int bit = 0;
+//
+//		for (int i = 0; i < M; i++) {
+//			st = new StringTokenizer(in.readLine());
+//			char oper = st.nextToken().charAt(1);
+//
+//			if (oper == 'l') {
+//				bit = (1 << 20) - 1;
+//			} else if (oper == 'm') {
+//				bit &= 0;
+//			} else {
+//				int x = Integer.parseInt(st.nextToken());
+//
+//				switch (oper) {
+//				case 'd':
+//					bit |= (1 << x - 1);
+//					break;
+//				case 'e':
+//					bit &= ~(1 << x - 1);
+//					break;
+//				case 'h':
+//					sb.append((bit & (1 << x - 1)) == 0 ? 0 : 1).append("\n");
+//					break;
+//				case 'o':
+//					bit ^= (1 << x - 1);
+//					break;
+//				}
+//			}
+//		}
+//
+//		System.out.println(sb.toString());
+//	}
+//
+//}
